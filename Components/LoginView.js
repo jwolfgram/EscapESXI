c063402ae6835992f8007029a358e9cf3ed26413
Joe Wolfgram <joseph.wolfgram@gmail.com>
2017-06-25 10:34:49 -0700

    Made changes for implementing a system menu for rebooting ESXI host.

--- Components/LoginView.js
+++ Components/LoginView.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   StyleSheet,
@@ -18,37 +18,64 @@ import {
   AsyncStorage,
   View,
   Keyboard
-} from 'react-native';
-import SSH from 'react-native-ssh';
-import ManageESXI from './manageESXI';
-import Spinner from 'react-native-loading-spinner-overlay';
-
+} from "react-native";
+import SSH from "react-native-ssh";
+import ManageESXI from "./manageESXI";
+import Spinner from "react-native-loading-spinner-overlay";
+import SystemMenuESXI from "./ModularComponents/SystemMenuESXI";
 
 export default class LoginView extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      userData:'',
-      userServerKey:''
-    }
+      userData: "",
+      userServerKey: ""
+    };
     this.loginAction = this.loginAction.bind(this);
   }
 
   componentDidMount() {
     console.log(this.props);
-    AsyncStorage.multiGet(['hostname', 'username', 'password']).then((data) => {
+    AsyncStorage.multiGet(["hostname", "username", "password"]).then(data => {
       console.log(data);
-      this.setState({hostname: data[0][1], username: data[1][1], password: data[2][1] })
-    })
+      this.setState({
+        hostname: data[0][1],
+        username: data[1][1],
+        password: data[2][1]
+      });
+    });
   }
 
   loginAction() {
-    this.setState({loggingIn: true});
-    if (this.state.hasOwnProperty('username') && this.state.hasOwnProperty('password') && this.state.hasOwnProperty('hostname')) {
-      Keyboard.dismiss()
-      AsyncStorage.multiSet([['hostname', this.state.hostname], ['username', this.state.username], ['password', this.state.password]])
+    this.setState({ loggingIn: true });
+    if (
+      this.state.hasOwnProperty("username") &&
+      this.state.hasOwnProperty("password") &&
+      this.state.hasOwnProperty("hostname")
+    ) {
+      Keyboard.dismiss();
+      AsyncStorage.multiSet([
+        ["hostname", this.state.hostname],
+        ["username", this.state.username],
+        ["password", this.state.password]
+      ]);
       console.log(this.props);
-      this.props.pushView(ManageESXI, {user: this.state.username, password: this.state.password, host: this.state.hostname}, "VMs");
+      this.props.pushView(
+        ManageESXI,
+        {
+          user: this.state.username,
+          password: this.state.password,
+          host: this.state.hostname,
+          rightBtn: (
+            <SystemMenuESXI
+              host={this.state.hostname}
+              user={this.state.username}
+              password={this.state.password}
+            />
+          )
+        },
+        "VMs"
+      );
     }
   }
 
@@ -74,156 +101,171 @@ Banner /etc/issue
 Subsystem sftp /usr/lib/vmware/openssh/bin/sftp-server -f LOCAL5 -l INFO
 AuthorizedKeysFile /etc/ssh/keys-%u/authorized_keys
 ClientAliveInterval 200' > /etc/ssh/sshd_config;`;
-    SSH.execute({
-      user: this.state.username,
-      password: this.state.password,
-      host: this.state.hostname
-    }, "mv /etc/ssh/sshd_config /etc/ssh/sshd_config.BK;" + sshConfigEcho).then(
+    SSH.execute(
+      {
+        user: this.state.username,
+        password: this.state.password,
+        host: this.state.hostname
+      },
+      "mv /etc/ssh/sshd_config /etc/ssh/sshd_config.BK;" + sshConfigEcho
+    ).then(
       result => console.log(result),
-      error =>  console.log('Error:', error)
+      error => console.log("Error:", error)
     );
   }
 
   showSSHFix() {
     if (this.state.loginTapCount >= 3) {
-      return (<View style={styles.bottomGroup}>
-                <TouchableHighlight style={styles.buttonBg} onPress={() => this.fixESXISSH()}>
-                  <Text style={styles.buttonText}>Cannot Login? Try this...</Text>
-                </TouchableHighlight>
-              </View>)
+      return (
+        <View style={styles.bottomGroup}>
+          <TouchableHighlight
+            style={styles.buttonBg}
+            onPress={() => this.fixESXISSH()}
+          >
+            <Text style={styles.buttonText}>Cannot Login? Try this...</Text>
+          </TouchableHighlight>
+        </View>
+      );
     }
   }
 
   render() {
     if (this.state.loggingIn) {
-      return <View style={{flex: 1,justifyContent: 'center',alignItems: 'center',}}>
-                <Spinner visible={true} textContent={"logging in..."}/>
-              </View>
+      return (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Spinner visible={true} textContent={"logging in..."} />
+        </View>
+      );
     }
     return (
       <View style={styles.container}>
         <View style={styles.inputGroup}>
           <Text style={styles.title}>EscapESXI</Text>
           <TextInput
-              style={styles.inputStyle}
-              autoCapitalize='none'
-              autoCorrect={false}
-              onChangeText={(text) => this.setState({hostname: text})}
-              value={this.state.hostname}
-              placeholder={"Hostname or IP Address"}
-              placeholderTextColor='grey'
-          />
-          <TextInput
-              style={styles.inputStyle}
-              autoCapitalize='none'
-              autoCorrect={false}
-              onChangeText={(text) => this.setState({username: text})}
-              value={this.state.username}
-              placeholder={"Username"}
-              placeholderTextColor='grey'
+            style={styles.inputStyle}
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={text => this.setState({ hostname: text })}
+            value={this.state.hostname}
+            placeholder={"Hostname or IP Address"}
+            placeholderTextColor="grey"
           />
           <TextInput
             style={styles.inputStyle}
-            autoCapitalize='none'
-            onChangeText={(text) => this.setState({password: text})}
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={text => this.setState({ username: text })}
+            value={this.state.username}
+            placeholder={"Username"}
+            placeholderTextColor="grey"
+          />
+          <TextInput
+            style={styles.inputStyle}
+            autoCapitalize="none"
+            onChangeText={text => this.setState({ password: text })}
             value={this.state.password}
             secureTextEntry={true}
             placeholder={"Password"}
-            placeholderTextColor='grey'
+            placeholderTextColor="grey"
           />
         </View>
         <View style={styles.bottomGroup}>
-          <TouchableHighlight style={[styles.btn, styles.buttonBg]} onPress={() => this.loginAction()}>
+          <TouchableHighlight
+            style={[styles.btn, styles.buttonBg]}
+            onPress={() => this.loginAction()}
+          >
             <Text style={styles.buttonText}>Login</Text>
           </TouchableHighlight>
         </View>
-      </View>);
+      </View>
+    );
   }
 }
 
-var {width} = Dimensions.get('window');
+var { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: width,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection:'column',
-
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
   },
   title: {
     fontSize: 64,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
-    color:'#FFF',
+    textAlign: "center",
+    color: "#FFF"
   },
-  bottomGroup:{
-    flex:2,
-    flexDirection:'column',
+  bottomGroup: {
+    flex: 2,
+    flexDirection: "column"
   },
   btn: {
     padding: 3,
     margin: 5,
-    justifyContent:'center',
-    alignItems:'center',
-    alignSelf:'center',
-    shadowColor: '#000000',
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    shadowColor: "#000000",
     shadowOffset: {
       width: 1,
       height: 1
     },
     shadowRadius: 1,
-    shadowOpacity: .8
+    shadowOpacity: 0.8
   },
-  inputGroup:{
-    flex:3,
+  inputGroup: {
+    flex: 3,
     paddingBottom: 20,
-    flexDirection:'column',
-    justifyContent: 'flex-end',
+    flexDirection: "column",
+    justifyContent: "flex-end"
   },
-  inputStyle:{
+  inputStyle: {
     height: 40,
-    width:width - 40,
+    width: width - 40,
     marginBottom: 10,
-    borderColor: 'grey',
+    borderColor: "grey",
     borderWidth: 0.5,
-    backgroundColor:'#FFFFFF',
-    textAlign:'center',
-    color:'grey'
+    backgroundColor: "#FFFFFF",
+    textAlign: "center",
+    color: "grey"
   },
-  loginGroup:{
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center',
+  loginGroup: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   loginGroupAccount: {
     fontSize: 16,
-    color:'#000000',
-    textAlign:'right',
+    color: "#000000",
+    textAlign: "right"
   },
   loginGroupBtnLabel: {
     fontSize: 16,
     margin: 20,
-    color:'rgb(0,179,227)',
-    textAlign:'left',
-    flex:1
+    color: "rgb(0,179,227)",
+    textAlign: "left",
+    flex: 1
   },
-  buttonBg:{
-    backgroundColor:'#009688',
-    width:220,
-    justifyContent:'center',
-    alignItems:'center',
-    alignSelf:'center',
-    height:50,
+  buttonBg: {
+    backgroundColor: "#009688",
+    width: 220,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    height: 50
   },
-  buttonText:{
-    fontSize:16,
-    justifyContent:'center',
-    color:'white',
-    alignSelf:'center',
-    fontWeight: 'bold',
+  buttonText: {
+    fontSize: 16,
+    justifyContent: "center",
+    color: "white",
+    alignSelf: "center",
+    fontWeight: "bold"
   }
 });
--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component, createElement, createClass } from 'react';
+import React, { Component, createElement, createClass } from "react";
 import {
   StyleSheet,
   Text,
@@ -12,8 +12,8 @@ import {
   Navigator,
   TouchableHighlight,
   Dimensions
-} from 'react-native';
-import LoginView from './Components/LoginView.js'
+} from "react-native";
+import LoginView from "./Components/LoginView.js";
 
 export default class EscapESXI extends Component {
   constructor(props) {
@@ -21,7 +21,7 @@ export default class EscapESXI extends Component {
     this.state = {
       views: [],
       currentCount: 0
-    }
+    };
     this.renderScene = this.renderScene.bind(this);
     this.renderNavigator = this.renderNavigator.bind(this);
     this.pushView = this.pushView.bind(this);
@@ -29,69 +29,99 @@ export default class EscapESXI extends Component {
   }
 
   componentWillMount() {
-    this.pushView(LoginView, {}, "Login")
+    this.pushView(LoginView, {}, "Login");
   }
 
   pushView(view, props, name) {
-    let stateViews = this.state.views
-    let currentView = stateViews[this.state.views.length - 1]
+    let stateViews = this.state.views;
+    let currentView = stateViews[this.state.views.length - 1];
 
     if (currentView && this.refs.current) {
-      currentView.state = this.refs.current.state
-      console.log(currentView)
+      currentView.state = this.refs.current.state;
+      console.log(currentView);
     }
 
-    let pushThis = stateViews.concat({Component: view, props: props, name: name});
-    this.setState({views: pushThis});
+    console.log(props);
+
+    let pushThis = stateViews.concat({
+      Component: view,
+      props: props,
+      name: name
+    });
+    this.setState({ views: pushThis });
   }
 
   goBack() {
     deleteView = this.state.views;
-    console.log(deleteView)
-    deleteView.pop()
-    this.setState({views: deleteView})
+    deleteView.pop();
+    this.setState({ views: deleteView });
   }
 
   renderNavigator() {
-    let currentView = this.state.views[this.state.views.length - 1]
-    return (<View style={styles.navbarView}>
-              { this.state.views.length > 1 ? <TouchableHighlight style={styles.leftNavButton} onPress={() => this.goBack()}><Text style={styles.leftNavButtonText}>{'<'}</Text></TouchableHighlight> : <Text style={styles.leftNavButtonText}>{'  '}</Text>}
-                <View  style={styles.titleView}>
-                  <Text style={styles.title}>{currentView.name}</Text>
-                </View>
-              <TouchableHighlight style={styles.rightNavButton} onPress={() => console.log('shiot')}><Text style={styles.rightNavButtonText}></Text></TouchableHighlight>
-            </View>)
+    let currentView = this.state.views[this.state.views.length - 1];
+    console.log(currentView);
+    return (
+      <View style={styles.navbarView}>
+        {this.state.views.length > 1
+          ? <TouchableHighlight
+              style={styles.leftNavButton}
+              onPress={() => this.goBack()}
+            >
+              <Text style={styles.leftNavButtonText}>{"<"}</Text>
+            </TouchableHighlight>
+          : <Text style={styles.leftNavButtonText}>{"  "}</Text>}
+        <View style={styles.titleView}>
+          <Text style={styles.title}>{currentView.name}</Text>
+        </View>
+        <View style={styles.rightNavButton}>
+          {currentView.props.rightBtn
+            ? currentView.props.rightBtn
+            : <Text>  </Text>}
+        </View>
+      </View>
+    );
   }
 
   renderScene() {
-    let currentView = this.state.views[this.state.views.length - 1]
-    let element = currentView.Component
+    let currentView = this.state.views[this.state.views.length - 1];
+    let element = currentView.Component;
     //SET INIT STATE HERE
     let elementFuncNames = Object.getOwnPropertyNames(element.prototype);
-    let newObject = { //We can add custom callback functions here such as goBack() for navigation
-                      getInitialState: function() {
-                        console.log(this)
-                        return currentView.state || {};
-                      }
-                    }
+    let newObject = {
+      //We can add custom callback functions here such as goBack() for navigation
+      getInitialState: function() {
+        console.log(this);
+        return currentView.state || {};
+      }
+    };
     for (let obj of elementFuncNames) {
-      if (obj == 'constructor') { //TODO
+      if (obj == "constructor") {
+        //TODO
       } else {
-        newObject[obj] = element.prototype[obj]
+        newObject[obj] = element.prototype[obj];
       }
     }
-    let classInstance = createClass(newObject)
-    let elementInstance = createElement(classInstance, {pushView: this.pushView, props: {}})
-    return <elementInstance.type ref="current" pushView={this.pushView} {...currentView.props}/>//element
+    let classInstance = createClass(newObject);
+    let elementInstance = createElement(classInstance, {
+      pushView: this.pushView,
+      props: {}
+    });
+    return (
+      <elementInstance.type
+        ref="current"
+        pushView={this.pushView}
+        {...currentView.props}
+      />
+    ); //element
   }
 
   render() {
     return (
       <View style={styles.container}>
-      {this.renderNavigator()}
-      <View style={styles.componentContainer}>
-        {this.renderScene()}
-      </View>
+        {this.renderNavigator()}
+        <View style={styles.componentContainer}>
+          {this.renderScene()}
+        </View>
       </View>
     );
   }
@@ -100,65 +130,62 @@ export default class EscapESXI extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#455A64',
-    flexDirection:'column',
+    backgroundColor: "#455A64",
+    flexDirection: "column"
   },
   componentContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 60,
     left: 0,
     right: 0,
     bottom: 0
   },
   navbarView: {
-    flexDirection:'row',
+    flexDirection: "row",
     height: 60,
-    justifyContent:'space-around',
-    alignItems:'center',
-    alignSelf:'center',
-    width: '100%',
+    justifyContent: "space-around",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "100%",
     paddingTop: 20,
     zIndex: 100,
-    backgroundColor: '#607D8B',
-    shadowColor: '#000000',
+    backgroundColor: "#607D8B",
+    shadowColor: "#000000",
     shadowOffset: {
       width: 1,
       height: 2
     },
     shadowRadius: 1,
-    shadowOpacity: .8
+    shadowOpacity: 0.8
   },
   titleView: {
-    flex: .8,
-    alignItems:'center',
-    alignSelf:'center',
+    flex: 0.8,
+    alignItems: "center",
+    alignSelf: "center"
   },
   title: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 24,
-    fontWeight: 'bold',
-
+    fontWeight: "bold"
   },
   rightNavButtonText: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 24,
-    marginRight: 20,
-
+    marginRight: 20
   },
   leftNavButtonText: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 24,
-    marginLeft: 20,
-
+    marginLeft: 20
   },
   rightNavButton: {
-    flex: .1,
-    alignItems:'center',
-    alignSelf:'center',
+    flex: 0.1,
+    alignItems: "center",
+    alignSelf: "center"
   },
   leftNavButton: {
-    flex: .1,
-    alignItems:'center',
-    alignSelf:'center',
-  },
+    flex: 0.1,
+    alignItems: "center",
+    alignSelf: "center"
+  }
 });
