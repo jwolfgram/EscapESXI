aebb99fbfabe82ba037101168787d97b0a53d48d
Joe Wolfgram <joseph.wolfgram@gmail.com>
2018-08-12 08:13:32 -0700

    New RN Project for refresh

--- index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import React, { AppRegistry } from 'react-native';
-import EscapESXI from './main.js';
-
-AppRegistry.registerComponent('EscapESXI', () => EscapESXI);
---
+++ ios/Pods/Target Support Files/Pods-EscSXI/Pods-EscSXI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EscSXI : NSObject
+@end
+@implementation PodsDummy_Pods_EscSXI
+@end
